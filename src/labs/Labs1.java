71112f1398e5fa723437cea07bd89d83d711128d
nitinsivav <nitinsiva.v@gmail.com>
2026-08-13 20:14:17 +0530

    Labs Ch 1

---
+++ src/labs/Labs1.java
@@ -0,0 +1,28 @@
+public class Labs1{
+    // lab A
+    public static void main(String[] args) {
+        int a=10;
+        long b=20;
+        double c=30.0;
+        float d=40.0f;
+        char e='A';
+        boolean f=true;
+
+        // lab B
+        final double VALUE_OF_PI=3.14;
+
+        // labs C
+        double value = 9.7;
+        int intValue = (int) value;
+        int intValue2 = (int) Math.round(value);
+
+        // lab D
+
+        int q = 5/2;
+
+        double a1 = (double) 5/2;
+
+        System.out.println("q: " + q);
+        System.out.println("a1: " + a1);
+    }
+}
