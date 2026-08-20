2aaad344a3a5faceee2c9e3df6ba52d842fda1af
nitinsivav <nitinsiva.v@gmail.com>
2026-08-20 18:09:40 +0530

    ass - ch 2

--- src/splitwise/Splitwise.java
+++ src/splitwise/Splitwise.java
@@ -1,5 +1,7 @@
 package splitwise;
 
+import java.util.Scanner;
+
 public class Splitwise {
     public static void main(String[] args) {
         System.out.println("=== Splitwise ===");
@@ -7,9 +9,37 @@ public class Splitwise {
         double totalAmount = 120.0;
         int numberOfPeople = 4;
         double amountPerPerson = totalAmount / numberOfPeople;
-        System.out.println("Description: " + description);
-        System.out.println("Total Amount: $" + totalAmount);
-        System.out.println("Number of People: " + numberOfPeople);
-        System.out.println("Amount Per Person: $" + amountPerPerson);
+        Scanner sc = new Scanner(System.in);
+        boolean running= true;
+        while (running) {
+
+            System.out.println("MENU");
+            System.out.println("1.Add Expense");
+            System.out.println("2.View Expense");
+            System.out.println("3.Exit");
+
+            int choice = sc.nextInt();
+
+            if(choice == 1){
+                System.out.println("Adding Expense....");
+                System.out.println("Added Successfully");
+            }
+            else if(choice == 2){
+                System.out.println("Expense Summary");
+                System.out.println("Description: " + description);
+                System.out.println("Total Amount: $" + totalAmount);
+                System.out.println("Number of People: " + numberOfPeople);
+                System.out.println("Amount Per Person: $" + amountPerPerson);
+            }
+            else if(choice ==3){
+                System.out.println("Goodbye");
+                running = false;
+
+            }
+            else{
+                System.out.println("Invalid Choice");
+            }
+
+        }
     }
 }
